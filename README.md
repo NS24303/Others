3adc0c7c466e1d4a45246e6938ae6c5c198fa10d
Nick <nstobbart@protonmail.com>
2022-05-23 18:02:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Other Scripts
+
+## BMI Script
+- calculates BMI for given height & weight + ranges for each category
+
---
+++ bmi.py
@@ -0,0 +1,7 @@
+
+height = input("Please enter your height (in cm, i.e. 180): ")
+weight = input("Please end your weight (in kg, i.e. 70.5kg): ")
+
+print("Your Height is: ", height, "\nYour Weight is", weight,"KG")
+
+
