a0bdc68fb6757b234646e7a9b26fad8815a612f5
Nick <nstobbart@protonmail.com>
2022-05-23 19:05:31 +0100

    formatting update

--- bmi.py
+++ bmi.py
@@ -2,16 +2,20 @@
 # Future improvement: validation of data
 # future improvement: Unit conversion
 
-height = float(input("Please enter your height (in meters, i.e. 1.80): "))
-weight = int(input("Please end your weight (in kg, i.e. 70.5kg): "))
+height = float(input("\nPlease enter your height (in meters, i.e. 1.80): "))
+weight = int(input("\nPlease end your weight (in kg, i.e. 70.5kg): "))
 
-print("Your Height is: ", height, "\nYour Weight is", weight,"KG")
+print("\n\n========")
+print("Your Height is:", height, "Meters \nYour Weight is:", weight,"KG")
+print("========")
 
 # calculate BMI and round to 1 digit
 # future improvement use maths module for powers?
 bmi = round(weight/(height**2),1)
 
-print("\n\nYour Current BMI is: " ,bmi)
+print("========")
+print("========")
+print("\nYour Current BMI is: " ,bmi, "\n")
 
 if (bmi >= 30):
     print("Obese range")
@@ -21,14 +25,13 @@ elif (bmi >=18.5):
     print("Normal/Healthy Weight range")
 else:
     print("Underweight Range")
-
+print("\n========")
+print("========\n")
 ### Next task, for a given height, calc a users weights for given BMI values.
 
 '''
-
     If your BMI is less than 18.5, it falls within the underweight range.
     If your BMI is 18.5 to 24.9, it falls within the normal or Healthy Weight range.
     If your BMI is 25.0 to 29.9, it falls within the overweight range.
     If your BMI is 30.0 or higher, it falls within the obese range.
-    
 '''
